8dbe181bf36caaf0b82792cb4abf2b0ee4fff1e8
mei5342 <mei13115342@gmail.com>
2022-04-29 11:36:41 +0900

    errors

--- exercise-stringer/exercise-stringer.go
+++ exercise-stringer/exercise-stringer.go
@@ -4,6 +4,10 @@ import "fmt"
 
 type IPAddr [4]byte
 
+func (a IPAddr) String() string {
+	return fmt.Sprintf("%d.%d.%d.%d", a[0], a[1], a[2], a[3])
+}
+
 func main() {
 	hosts := map[string]IPAddr {
 		"loopback": {127, 0, 0, 1},
